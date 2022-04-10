1f57b7871749e57704b8e0b82425d0f6e72098b0
Dave Dribin <dave-vcs@dribin.org>
2022-04-09 22:29:17 -0500

    Switch to SK9822 on pins 11/13

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 
 
 #define NUM_LEDS 30
-#define DATA_PIN 10
+#define DATA_PIN 11
+#define CLOCK_PIN 13
 
 #define USE_SERIAL 0
 
@@ -32,7 +33,7 @@ void setup()
     pinMode(PIN_BTN1, INPUT_PULLUP);
     pinMode(PIN_BTN2, INPUT_PULLUP);
 
-    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.addLeds<SK9822, DATA_PIN, CLOCK_PIN, RGB, DATA_RATE_KHZ(100)>(leds, NUM_LEDS);
     FastLED.setBrightness(84);
 }
 
